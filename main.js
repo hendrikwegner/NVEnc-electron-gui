9fdd1f93a6ac8be0499965d5fd6ddf7be8de604b
Hendrik Wegner <hendrik.wegner@rhiem.com>
2021-03-19 23:32:12 +0100

    initial file dump

---
+++ main.js
@@ -0,0 +1,75 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, dialog, ipcMain } = require('electron')
+const path = require('path')
+const {spawn} = require("child_process");
+/*
+try {
+require('electron-reloader')(module)
+} catch (_) {
+}
+*/
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    frame: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.removeMenu()
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  return mainWindow
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  const window = createWindow()
+
+
+  ipcMain.on('saveTo_ToMain', (event, msg) => {
+    console.log(msg) // msg from web page
+
+    const savePath = dialog.showOpenDialog({
+      title: "Select a folder",
+      properties: ["openDirectory"]
+    }).then(result => {
+      window.webContents.send('saveTo_ToPreload', result) // send to web page
+    }).catch(err => {
+      window.webContents.send('saveTo_ToPreload', err) // send to web page
+
+    })
+
+
+  })
+
+
+
+
+
+  app.on('activate', function () {
+
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ preload.js
@@ -0,0 +1,111 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+const {execFile} = require("child_process");
+const { ipcRenderer } = require('electron');
+
+function init() {
+    // add global variables to your web page
+    window.isElectron = true
+    window.ipcRenderer = ipcRenderer
+    window.saveTo = ""
+    window.filePaths = []
+    window.logs = []
+
+
+    window.ipcRenderer.on('saveTo_ToPreload', (event, msg) => {
+        console.log(msg)
+        window.saveTo = msg.filePaths[0] + "\\"
+
+    })
+}
+
+init();
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const btn = document.getElementById('saveTo')
+    btn.addEventListener('click', (e) => {
+
+        if (window.isElectron) {
+            window.ipcRenderer.send('saveTo_ToMain', 'select save folder.')
+        }
+    })
+
+    const drop = document.getElementById('dropzone')
+    drop.ondrop = (ev) => {
+        ev.preventDefault()
+
+        for (let file of ev.dataTransfer.files){
+            window.filePaths.push(file.path)
+        }
+        console.log(window.filePaths)
+
+        const params = document.getElementById('params')
+        paramsArray = params.value.split(' ')
+
+        encode(window.filePaths.shift(), paramsArray,"_")
+    }
+})
+
+updateLog = function (data) {
+    window.logs.push(data)
+    log = document.getElementById('log')
+
+    var entry = document.createElement('pre');
+    entry.className = 'log-entry';
+    entry.innerHTML = data;
+
+    log.prepend(entry)
+}
+
+encode = function (pathToFile, params, prefix = "", suffix = "") {
+    let filename = pathToFile.split('\\')
+    filename = filename[filename.length - 1].replace(/\.[^/.]+$/, "")
+
+    params.push('-i')
+    params.push(pathToFile)
+    params.push('-o')
+    params.push(window.saveTo+prefix + filename + suffix + ".mp4")
+
+    const runner = execFile(`NVEncC64.exe`, params, {cwd: './NVEncC_5.29_x64/'});
+    console.log(runner)
+
+    runner.stdout.on('data', function (data) {
+        console.log('stdout: ' + data.toString());
+        updateLog(data.toString())
+    });
+
+    runner.stderr.on('data', function (data) {
+        console.log('stderr: ' + data.toString());
+        updateLog(data.toString())
+
+    });
+
+    runner.on('exit', function (code) {
+        console.log('child process exited with code ' + code.toString());
+        updateLog('child process exited with code ' + code.toString())
+
+
+
+        if (code == 0) {
+            console.log('success...?')
+            if (window.filePaths.length > 0) {
+                encode(window.filePaths.shift(), "hevc", "_")
+                console.log('filepaths:', window.filePaths)
+            }
+        }
+    });
+}
+
+
+getDateString = function () {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    return `${year}${month}${day}`
+}
